5529580813340b76dccec42e4121aa9574a75694
Prince <kumabio.prince@student.moringaschool.com>
2022-10-25 15:39:42 +0000

    completed

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,9 @@ class Student < ApplicationRecord
   def to_s
     "#{first_name} #{last_name}"
   end
-
+ 
+  def show
+     student = Student.find_by(id: params[:id])
+     render json: student
+  end
 end
